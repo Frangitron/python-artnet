7cd348c19561c0dcae590f3580048aa7dc5e3bb5
MrFrangipane <contact@frangipane.me>
2025-12-10 11:58:39 +0100

    feat(Broadcaster): Adds bind_address parameter to constructor to select network interface

--- pythonartnet/broadcaster.py
+++ pythonartnet/broadcaster.py
@@ -11,15 +11,15 @@ class ArtnetBroadcastError(OSError):
 class ArtnetBroadcaster:
     UDP_PORT = 6454
 
-    def __init__(self, target_ip):
+    def __init__(self, target_ip: str, bind_address: str | None = None):
         self.target_ip = target_ip
         self.universes: dict[int, ArtnetUniverse] = dict()
 
         self._socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self._socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        if bind_address is not None:
+            self._socket.bind((bind_address, 0))
         self._socket.settimeout(1)  # todo check if OK
-
-        print(f"Artnet broadcaster created, target ip {target_ip}")
 
     def add_universe(self, universe_number: int):
         if universe_number in self.universes:
@@ -28,12 +28,9 @@ class ArtnetBroadcaster:
         self.universes[universe_number] = ArtnetUniverse(universe_number)
 
     def send_data(self):
-        try:
-            for universe in self.universes.values():
-                packet_ = universe.make_packet()
-                self._socket.sendto(packet_, (self.target_ip, self.UDP_PORT))
-        except OSError:
-            raise ArtnetBroadcastError(f"Impossible to send to {self.target_ip}")
+        for universe in self.universes.values():
+            packet = universe.make_packet()
+            self._socket.sendto(packet, (self.target_ip, self.UDP_PORT))
 
     def send_artsync(self):
         self._socket.sendto(packet.ARTSYNC_PACKET, (self.target_ip, self.UDP_PORT))
